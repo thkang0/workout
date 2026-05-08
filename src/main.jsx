87a1e6aa1e58177c627f14985a72bc9671493633
thkang0 <thkang0@gmail.com>
2026-05-09 07:57:37 +0900

    GitHub Actions 배포 설정 추가

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TennisWorkoutGuide from './tennis_workout_guide.jsx' // 올려주신 파일 불러오기
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TennisWorkoutGuide />
+  </React.StrictMode>,
+)
